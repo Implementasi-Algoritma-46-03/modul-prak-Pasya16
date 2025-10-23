379c4face1907587e6aef95389eb5e218902381d
PasyaMR <Pasyamuhammadrizky@gmail.com>
2025-10-23 16:50:07 +0700

    pasyaa

--- Modul06/Jurnal01.java
+++ Modul06/Jurnal01.java
@@ -2,54 +2,31 @@ import java.util.Scanner;
 
 public class Jurnal01 {
 
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        String nama1 = s.nextLine();
-        String nama2 = s.nextLine();
-        int jam = s.nextInt();
-
-        System.out.println("Halo, " + nama1 + ". Selamat " + sapaan(jam) + ".");
-        System.out.println("Halo, " + nama2 + ". Selamat " + sapaan(jam) + ".");
-    }
-    
-    private static String sapaan (int jam){
-        String waktu = "";
-        if (jam >= 6 && jam <= 11){
-            waktu = "pagi";
-        } else if (jam >= 12 && jam <= 14){
-            waktu = "siang";
-        }else if (jam >= 15 && jam <= 17){
-            waktu = "sore";
-        }else if (jam >= 18 && jam <= 05){
-            waktu = "malam";
+    // Method untuk menentukan sapaan berdasarkan jam
+    public static String sapaan(int jam) {
+        if (jam >= 6 && jam <= 11) {
+            return "pagi";
+        } else if (jam >= 12 && jam <= 14) {
+            return "siang";
+        } else if (jam >= 15 && jam <= 17) {
+            return "sore";
+        } else {
+            return "malam";
         }
-        return waktu; 
-    }
-}mport java.util.Scanner;
-
-public class Jurnal01 {
-
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        String nama1 = s.nextLine();
-        String nama2 = s.nextLine();
-        int jam = s.nextInt();
-
-        System.out.println("Halo, " + nama1 + ". Selamat " + sapaan(jam) + ".");
-        System.out.println("Halo, " + nama2 + ". Selamat " + sapaan(jam) + ".");
     }
-    
-    private static String sapaan (int jam){
-        String waktu = "";
-        if (jam >= 6 && jam <= 11){
-            waktu = "pagi";
-        } else if (jam >= 12 && jam <= 14){
-            waktu = "siang";
-        }else if (jam >= 15 && jam <= 17){
-            waktu = "sore";
-        }else if (jam >= 18 && jam <= 05){
-            waktu = "malam";
-        }
-        return waktu; 
-    }
+
+    // Method utama    
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String nama1 = input.nextLine();
+        String nama2 = input.nextLine();
+        int jam = input.nextInt();
+        String hasilJam = sapaan(jam);
+
+        System.out.println("Halo, " + nama1 + ". Selamat " + hasilJam + ".");
+        System.out.println("Halo, " + nama2 + ". Selamat " + hasilJam + ".");
+
+        input.close();
+   }
 }
