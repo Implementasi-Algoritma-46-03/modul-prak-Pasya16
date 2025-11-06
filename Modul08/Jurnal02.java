33151171e6e37ac705bc07776ab3cf951295f0ae
PasyaMR <Pasyamuhammadrizky@gmail.com>
2025-11-06 17:32:49 +0700

    Jurnal02 & Jurnal03

--- Modul08/Jurnal02.java
+++ Modul08/Jurnal02.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class Jurnal02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+        int jumlah = 0;
+        int hitung = 0;
+
+        while (true){
+            int n = input.nextInt();
+            if (n == 0) break;
+            jumlah += n;
+            hitung++;
+        }
+
+        double rata = (double) jumlah / hitung;
+        System.out.printf("%.2f\n", rata);
     }
 }
--- Modul08/Jurnal03.java
+++ Modul08/Jurnal03.java
@@ -1,6 +1,30 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class Jurnal03 {
+    public static int faktorial(int n) {
+        int hasil = 1;
+        for (int i = 1; i <= n; i++) {
+            hasil *= i;
+        }
+        return hasil;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int angka = input.nextInt();
+        int temp = angka;
+        int jumlah = 0;
+
+        while (temp > 0) {
+            int digit = temp % 10;
+            jumlah += faktorial(digit);
+            temp /= 10;
+        }
+
+        if (jumlah == angka) {
+            System.out.println("YA");
+        } else {
+            System.out.println("BUKAN");
+        }
     }
 }
