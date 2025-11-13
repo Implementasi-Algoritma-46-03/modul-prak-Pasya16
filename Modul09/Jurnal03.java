43716ece8066910f21733d6abd5714f6d7fe84f1
PasyaMR <Pasyamuhammadrizky@gmail.com>
2025-11-13 16:55:32 +0700

    perbaikan

--- Modul09/Jurnal03.java
+++ Modul09/Jurnal03.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 
 public class Jurnal03 {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int N = input.nextInt(); 
-        boolean prima = true;    
+        int N = input.nextInt();
+        boolean prima = true;
 
         if (N <= 1) {
             prima = false;
         } else {
-            
             for (int i = 2; i <= Math.sqrt(N); i++) {
-                if (N % i == 0) { 
+                if (N % i == 0) {
                     prima = false;
-                    break; 
+                    break;
                 }
             }
         }
 
         if (prima) {
-            System.out.println("YA");
+            System.out.print("YA");
         } else {
-            System.out.println("BUKAN");
+            System.out.print("BUKAN");
         }
+
+        input.close();
     }
 }
