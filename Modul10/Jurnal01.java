7cd05ee62443f9060bae88402b477d58ebbe2d23
PasyaMR <Pasyamuhammadrizky@gmail.com>
2025-11-20 16:58:47 +0700

    Jurnal 10

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -1,6 +1,19 @@
 public class Jurnal01 {
+    public static void main(String[] args) {
+        //untuk bagian baris, berapa angka yang di tampilin
+        for (int i = 1; i <= 6; i++) {
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+            //untuk mencari barisan genap
+            if (i % 2 == 0) {
+                System.out.print(" "); 
+            }
+
+            //untuk bagian jumlah angka tiap baris
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(i + " ");  
+            }
+
+            System.out.println(); 
+        }
     }
 }
