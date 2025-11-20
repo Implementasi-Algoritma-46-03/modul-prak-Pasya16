54a6c65f5a5e815011f080723658ee1942afa87f
PasyaMR <Pasyamuhammadrizky@gmail.com>
2025-11-20 17:01:36 +0700

    bismillah jurnal01

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -1,6 +1,5 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-        //untuk bagian baris, berapa angka yang di tampilin
         for (int i = 1; i <= 6; i++) {
 
             //untuk mencari barisan genap
@@ -8,9 +7,13 @@ public class Jurnal01 {
                 System.out.print(" "); 
             }
 
-            //untuk bagian jumlah angka tiap baris
             for (int j = 1; j <= 5; j++) {
-                System.out.print(i + " ");  
+                if (j == 5 && i % 2 == 0) {
+                    System.out.print(i);
+                }
+                else {
+                    System.out.print(i + " ");
+                }
             }
 
             System.out.println(); 
